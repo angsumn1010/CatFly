0d416d6bbbffd186873017fc9761c71373db2329
royangsuman <roy.angsuman@gmail.com>
2025-11-29 03:32:17 -0600

    Frame details updated for participant

--- routes/c_events.js
+++ routes/c_events.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-
 // helper to normalize date input (array / JSON string / single string)
 function normalizeDate(input) {
   if (!input) return null;
